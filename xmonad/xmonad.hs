673f7ab814b07c8845b3218b4c3745361f071a8b
Ricky Hariady <ricky.hariady@tech.jago.com>
2025-01-05 15:43:53 +0700

    change xmonad terminal to ghostty

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -28,7 +28,7 @@ import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
-myTerminal      = "urxvt"
+myTerminal      = "ghostty"
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
